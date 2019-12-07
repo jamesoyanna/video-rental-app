517b5efc929acfc6b3cb1d089d38753e2a38bd5d
eshoiza <engrjayt200@gmail.com>
2019-12-07 06:49:02 +0100

    added React.Fragment

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 
 
-
 export class Counter extends Component {
     render() {
         return (
-            <div>
+            <React.Fragment>
                 <h2>Hello Yetunde</h2>
                 <button className="btn">Increment</button>
-            </div>
+            </React.Fragment>
         );
     }
 }
