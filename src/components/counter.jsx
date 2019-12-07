93b29fb9b11a1b4b54aa99e3b12cbab89c46f84f
eshoiza <engrjayt200@gmail.com>
2019-12-07 07:22:56 +0100

    object destucturing

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 
 
 export class Counter extends Component {
+    state = {
+        count:1
+    }
     render() {
         return (
             <React.Fragment>
-                <h2>Hello Yetunde</h2>
-                <button className="btn">Increment</button>
+                <span>{     this.state.count}</span>
+    <button className="btn">Increment</button>
             </React.Fragment>
         );
     }
+   formaCount(){
+       const {count} = this.state
+       return count === 0 ? "Zero": count;
+   }
+
 }
 
 export default Counter;
