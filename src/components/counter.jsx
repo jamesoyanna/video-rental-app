a16b17c0964399d0bc5ae305240c7244718c3c1d
eshoiza <engrjayt200@gmail.com>
2019-12-07 22:03:48 +0100

    rendering style from state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,15 +3,16 @@ import React, { Component } from 'react';
 
 export class Counter extends Component {
     state = {
-        count:1,
-        imgUrl: "https://media.gettyimages.com/photos/tokyo-skyscrapper-at-night-picture-id1165888881?s=612x612"
+        count:1
     }
+ 
+    
     render() {
         return (
             <div>
-                <img src={this.state.imgUrl} alt=""/>
-                <span>{this.formatCount()}</span>
-    <button className="btn">Increment</button>
+                
+         <span className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
     }
