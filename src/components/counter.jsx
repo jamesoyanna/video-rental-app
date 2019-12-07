3a53b8120ca2ace5a535f688a8e14b990049fbc3
eshoiza <engrjayt200@gmail.com>
2019-12-07 06:33:48 +0100

    created a counter component

---
+++ src/components/counter.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+
+export class Counter extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Hello Yetunde</h2>
+                <button className="btn">Increment</button>
+            </div>
+        );
+    }
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import Teacher from './Teacher';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/counter"
+import * as serviceWorker from "./serviceWorker";
 
-//const person = new Person('Mosh');
+ReactDOM.render(<Counter />, document.getElementById("root"));
 
-
-
-const teacher = new Teacher('mosh', 'MSC')
-teacher.teach();
-
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
